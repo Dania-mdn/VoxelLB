6217aa42e2f16fd35d74892251aa082abad79172
Dania-mdn <58430901+Dania-mdn@users.noreply.github.com>
2023-12-07 11:51:44 +0200

    sadasd
    ssad

--- Voxel/Assets/Player/Scripts/BodyHendler.cs
+++ Voxel/Assets/Player/Scripts/BodyHendler.cs
@@ -8,27 +8,81 @@ public class BodyHendler : MonoBehaviour
 
     private Animator animator;
 
+    public GameObject SwordHand;
+    public GameObject BowHand;
+    public GameObject Swordback;
+    public GameObject Bowback;
+
+    public GameObject BowrLeftHand;
+    private bool SwordReady = true;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+
+        if(SwordReady)
+        {
+            SwordHand.SetActive(true);
+            BowrLeftHand.SetActive(false);
+
+            Swordback.SetActive(false);
+            Bowback.SetActive(true);
+        }
+        PlayerPrefs.SetInt("NumberWeapon", 1);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.D) == false)
+        if (PlayerPrefs.GetInt("NumberWeapon") == 1)
         {
-            cut.cut = true;
-            animator.Play("attack");
+            if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.D) == false)
+            {
+                cut.cut = true;
+                animator.Play("attack");
+            }
+            else if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.A) == true)
+            {
+                cut.cut = true;
+                animator.Play("left");
+            }
+            else if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.D) == true)
+            {
+                cut.cut = true;
+                animator.Play("right");
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.A) == true)
+        else if(PlayerPrefs.GetInt("NumberWeapon") == 2)
         {
-            cut.cut = true;
-            animator.Play("left");
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                animator.Play("BowAttack");
+            }
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                animator.SetBool("BowFire", false);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.D) == true)
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            cut.cut = true;
-            animator.Play("right");
+            if(PlayerPrefs.GetInt("NumberWeapon") == 2)
+            {
+                if (BowrLeftHand.activeSelf == true)
+                {
+                    BowHand.SetActive(true);
+                    BowrLeftHand.SetActive(false);
+                }
+                animator.Play("chanche");
+                PlayerPrefs.SetInt("NumberWeapon", 1);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (PlayerPrefs.GetInt("NumberWeapon") == 1)
+            {
+                animator.Play("chanche");
+                PlayerPrefs.SetInt("NumberWeapon", 2);
+            }
         }
     }
     public void SetCutfalse()
@@ -38,5 +92,38 @@ public class BodyHendler : MonoBehaviour
     public void SetCuttrue()
     {
         cut.cut = true;
+    }
+    public void SetChanche()
+    {
+        if (SwordReady)
+        {
+            SwordHand.SetActive(false);
+            BowHand.SetActive(true);
+
+            Swordback.SetActive(true);
+            Bowback.SetActive(false);
+        }
+        else
+        {
+            SwordHand.SetActive(true);
+            BowHand.SetActive(false);
+
+            Swordback.SetActive(false);
+            Bowback.SetActive(true);
+        }
+        SwordReady = !SwordReady;
+    }
+
+    public void SetBowReadyFire()
+    {
+        animator.SetBool("BowFire", true);
+    }
+    public void SetchancheHendBow()
+    {
+        if(BowHand.activeSelf == true)
+        {
+            BowHand.SetActive(false);
+            BowrLeftHand.SetActive(true);
+        }
     }
 }
